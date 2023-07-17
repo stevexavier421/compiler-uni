9098dff739db64a7dc2e06b9d7d6b072172037f8
stevexavier421 <139811559+stevexavier421@users.noreply.github.com>
2023-07-18 01:27:34 +0530

    Create main.c

---
+++ 10/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int n = 100; 
+    int sum = 0; 
+
+    // Enable parallelism and distribute the loop iterations across multiple threads
+    #pragma omp parallel for reduction(+:sum)
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
